814b4fa66d43fd0aa50cc93d71bef19d6cb8c80e
Low Wei Feng <158588461+monitorseer@users.noreply.github.com>
2024-08-19 00:05:09 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    # Run the app on host 0.0.0.0 and the specified port
+    app.run(host='0.0.0.0', port=port)
