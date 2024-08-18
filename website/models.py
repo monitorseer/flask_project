47a0df0a02dd60ba782b2850cb08c6fd79d17779
Low Wei Feng <158588461+monitorseer@users.noreply.github.com>
2024-08-18 23:18:34 +0800

    Added all basic functions
    Would like to have added additional quality of life updates that would have taken a lot more time. Also wish I got my CSS for the homepage working as intended but this can settle for now.

--- website/models.py
+++ website/models.py
@@ -6,9 +6,11 @@ from sqlalchemy import func
 # Stores information about notes
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100)) # Specifies whether it is Note or Work Reminder
     title = db.Column(db.String(100))
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now()) # Timezone is based on user's timezone
+    deadline = db.Column(db.DateTime(timezone=True), nullable=True) # Stores deadline for note
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # Connects note to user
 
 # Stores informaton about user
@@ -17,4 +19,5 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True) # Only one user can have one email at a time
     password = db.Column(db.String(128))
+    timetable_path = db.Column(db.String(150), nullable=True) # Stores path to timetable
     notes = db.relationship('Note') # Connects user to notes
--- website/views.py
+++ website/views.py
@@ -1,23 +1,32 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from werkzeug.utils import secure_filename
+from .models import Note, User
 from . import db
+from datetime import datetime
 import json
+import os
+import requests
 
 views = Blueprint('views', __name__)
 
+# Main Homepage
 @views.route('/', methods=["GET", "POST"])
 @login_required
 def home():
     user_notes = Note.query.filter_by(user_id=current_user.id).all() # Retrieves all notes from user
     return render_template("home.html", user=current_user, notes=user_notes) # Passes notes to dashboard
 
+
+# Notes Page
 @views.route('/notes', methods=["GET", "POST"])
 @login_required
 def notes():
     if request.method == "POST":
+        category = request.form.get('category')
         title = request.form.get('title')
         note = request.form.get('note')
+        deadline = request.form.get('deadline')
 
         if len(note) < 1:
             flash('Note is too short!', category='error')
@@ -26,8 +35,12 @@ def notes():
         elif len(title) > 100:
             flash('Title is too long!', category='error')
         else:
+            # Convert deadline to datetime object if it exists
+            deadline_dt = None
+            if deadline:
+                deadline_dt = datetime.strptime(deadline, '%Y-%m-%d')
             # Adds note to DB
-            new_note = Note(data=note, title=title, user_id=current_user.id)
+            new_note = Note(category=category, title=title, data=note, deadline=deadline_dt, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
@@ -47,3 +60,112 @@ def delete_note():
             db.session.commit()
 
     return jsonify({}) 
+
+
+# Details Page
+@views.route('/details')
+@login_required
+def details():
+    user_details = User.query.filter_by(id=current_user.id).first() # Retrieves user details
+    return render_template("details.html", user=current_user)
+
+
+# Timetable Page
+# Ensure the upload folder exists
+UPLOAD_FOLDER = os.path.join('website', 'static', 'uploads') # Specifies the folder to store uploaded files
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER) # Creates folder if it doesn't exist
+
+@views.route('/timetable', methods=["GET", "POST"])
+@login_required
+def timetable():
+    file_path = None
+    if request.method == 'POST':
+        # Ensures that a file is uploaded
+        if 'image' not in request.files:
+            flash('No file part', category='error')
+            return redirect(request.url)
+        
+        file = request.files['image']
+
+        if file.filename == '':
+            flash('No selected file', category='error')
+            return redirect(request.url)
+
+        # Validate file extension
+        if not file.filename.endswith('.htm'):
+            flash('Invalid file type. Please upload an .htm file.', category='error')
+            return redirect(request.url)
+        
+        # If file exists, save it to the uploads folder
+        if file:
+            filename = secure_filename(file.filename) # Ensures that the filename is safe
+            file_path = os.path.join(UPLOAD_FOLDER, filename) # Construct file path
+            file.save(file_path) # Saves file to folder
+            file_path = os.path.normpath(file_path).replace('\\', '/') # Normalize the file path and replace backslashes with forward slashes
+            flash('File uploaded!', category='success')
+            # Updates DB with path
+            current_user.timetable_path = filename
+            db.session.commit()
+            print(file_path)
+       
+    if current_user.timetable_path: # Renders iframe if path exists
+        file_path = current_user.timetable_path
+        
+    return render_template("timetable.html", user=current_user, file_path=file_path)
+
+
+# Weather Page
+@views.route('/weather')
+@login_required
+def weather():
+    # Connects to OpenWeatherMap API for
+    city_name = 'Singapore'
+    API_Key = '663b69c14e4c025c11e8e8e28e7880a5'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}&units=metric'
+
+    response = requests.get(url) # Sends GET request to API
+    data = response.json() # Converts response to dictionary
+
+    # Extracts relevant data from dictionary
+    longitude = data['coord']['lon']
+    latitude = data['coord']['lat']
+    weather = data['weather'][0]['main']
+    weather_description = data['weather'][0]['description']
+    temp = data['main']['temp']
+    feels_like = data['main']['feels_like']
+    min_temp = data['main']['temp_min']
+    max_temp = data['main']['temp_max']
+    pressure = data['main']['pressure']
+    humidity = data['main']['humidity']
+    wind_speed = data['wind']['speed']
+    wind_direction = data['wind']['deg']
+
+    # Used to display relevant data on website
+    data_list = [longitude, latitude, weather, weather_description, temp, feels_like, min_temp, max_temp, pressure, humidity, wind_speed, wind_direction]
+
+    # Connects to Data.gov.sg API for weather forecast
+    sg_url = 'https://api-open.data.gov.sg/v2/real-time/api/four-day-outlook'
+    sg_response = requests.get(sg_url) # Sends get request to API
+    sg_data = sg_response.json() # Converts response to dictionary
+
+    # Extracts relevant data from dictionary
+    days = [forecast['day'] for forecast in sg_data['data']['records'][0]['forecasts']]
+    temperature = [forecast['temperature'] for forecast in sg_data['data']['records'][0]['forecasts']]
+    weather_conditions = [forecast['forecast']['text'] for forecast in sg_data['data']['records'][0]['forecasts']]
+    sg_list = list(zip(days, temperature, weather_conditions))
+    return render_template("weather.html", user=current_user, data=data_list, sg_data=sg_list)
+
+
+# Calendar Page
+@views.route('/calendar')
+@login_required
+def calendar():
+    return render_template("calendar.html", user=current_user)
+
+
+# Links Page
+@views.route('/links')
+@login_required
+def links():
+    return render_template("links.html", user=current_user)
